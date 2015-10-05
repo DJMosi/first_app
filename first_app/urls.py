6897a4496057db0343860031b45dbf470a3ff203
DJMosi <davimosi@gmail.com>
2015-10-05 15:13:07 +0300

    first_app update

---
+++ first_app/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+     url(r'^$','home.views.index', name='home'),
+
+     url(r'^admin/', include(admin.site.urls)),
+)
+
+    # Examples:
+    #url(r'^$', 'first_app.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
