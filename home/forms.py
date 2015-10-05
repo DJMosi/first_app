b4842e5b1c4e2a30ee2be2fe8ae8f3d4019571f5
DJMosi <davimosi@gmail.com>
2015-10-05 18:20:04 +0300

    debugging progress

--- home/forms.py
+++ home/forms.py
@@ -6,11 +6,11 @@ class StudentForm(forms.ModelForm):
     class Meta:
         fields = ['full_name', 'email']
         model = Student
-
     def clean_age(self):
         age = self.cleaned_data.get('age')
         if age > 120:
             raise forms.ValidationError('You are too old for this class')
+
         elif age < 10:
             raise forms.ValidationError('You may be too young')
 
