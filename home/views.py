2204db2af4543bf8d55a7b108c36135d63ff4207
DJMosi <davimosi@gmail.com>
2015-10-06 09:28:33 +0300

    forms init

--- home/views.py
+++ home/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+#from django.http import HttpResponse
+from .forms import StudentForm
 # Create your views here.
 
 
 def index(request):
+    form = StudentForm()
+
     context = {
-        "hello_message": "Hello Moringa"
+        "hello_message": "Hello Moringa",
+        "form": form
     }
     return render(request, 'index.html', context)
 
