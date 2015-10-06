7dcc2d1a2fe461b74a32ea6971a6fb095dbdf24f
DJMosi <davimosi@gmail.com>
2015-10-06 15:03:02 +0300

    views perf

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from .forms import StudentForm
 
 
 def index(request):
-    print(request.Post)
+    print(request.POST  )
     form = StudentForm()
     context = {
         "hello_message": "Hello Moringa",
