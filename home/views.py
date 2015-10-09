0da7cead5b69bd5f23e442d533ec67344e77dc28
DJMosi <davimosi@gmail.com>
2015-10-09 13:38:10 +0300

    cleaned data

--- home/views.py
+++ home/views.py
@@ -20,16 +20,22 @@ def index(request):
         instance.full_name = full_name
         instance.save()
         context = {
-        "hello_message": "Student Saved",
+			"hello_message": "Student Saved",
 
     }
     return render(request, 'index.html', context)
 
 
+
 def feedback(request):
     form = FeedbackForm(request.POST or None)
     if form.is_valid():
-        for key, value in form.cleaned_data.items():
+		from_email = form.cleaned_data.get('email ')
+		full_name = form.cleaned_data.get('full_name')
+		message = form.cleaned_data.get('message')
+		prepared_message = "You have feedback from {} saying '{}'" .format(full_name, message)
+		send_mail('New feedback given ', 'prepared_message', 'from_email', ['davimosi@gmail.com'], fail_silently=False)
+    for key, value in form.cleaned_data.items():
             print(key, value)
     context = {
         "form": form
