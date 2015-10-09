8b2d81bac241f2a05456878e2a86ab7e136466bd
DJMosi <davimosi@gmail.com>
2015-10-09 13:36:36 +0300

    url patterns

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,14 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-admin.autodiscover()
+from django.conf import settings
+from django.conf.urls.static import static
+#admin.autodiscover()
 
-urlpatterns = patterns('',
-     url(r'^$','home.views.index', name='home'),
-     url(r'^feedback/$','home.views.feedback', name='feedback'),
-     url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = patterns['',
+					   url(r'^$', 'home.views.index', name='home'),
+					   url(r'^feedback/$', 'home.views.feedback', name='feedback'),
+					   url(r'^admin/', include(admin.site.urls)),
+]
 
-    # Examples:
-    #url(r'^$', 'first_app.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+# Examples:
+# url(r'^$', 'first_app.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
