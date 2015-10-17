1a9a3b51d2e43a257a71c253e54451aacfe4056f
DJMosi <davimosi@gmail.com>
2015-10-17 20:29:19 +0300

    students debuged

--- home/views.py
+++ home/views.py
@@ -5,26 +5,30 @@ from .models import students
 # Create your views here.
 
 def index(request):
-    form = StudentForm(request.POST or None)
+	return render(request, 'bootstrap_index.html', context)
 
-    context = {
-        "hello_message": "Register new student",
-        "form": form
-    }
 
-    if form.is_valid():
+def register(request):
+	form = StudentForm(request.POST or None)
 
-        instance = form.save(commit=False)
-        full_name = form.cleaned_data.get('full_name')
-        if full_name == "Jacob":
-            full_name = "Developer"
-        instance.full_name = full_name
-        instance.save()
-        context = {
+	context = {
+		"hello_message": "Register new student",
+		"form": form
+	}
+
+	if form.is_valid():
+
+		instance = form.save(commit=False)
+		full_name = form.cleaned_data.get('full_name')
+		if full_name == "Jacob":
+			full_name = "Developer"
+		instance.full_name = full_name
+		instance.save()
+		context = {
 			"hello_message": "Student Saved",
 
-    }
-    return render(request, 'index.html', context)
+	}
+	return render(request, 'index.html', context)
 
 
 
@@ -44,7 +48,7 @@ def feedback(request):
     return render(request, 'feedback.html', context)
 
 def Students(request):
-	search_term = request.GET['search']
+	search_term = request.GET.get('search',defult='')
 	students= Students.objects.all().order_by(-'last_update').filter(full_name__contains='search_term')
 	context = {'students': students}
 	return render(request, 'students.html', context)
